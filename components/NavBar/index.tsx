01a395076afa15e9dd83562229d20dccd4fdb060
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-10-24 09:00:12 -0300

    Refactor NavBar component to improve expand/collapse functionality

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -37,84 +37,92 @@ export const NavBar = ({ where }: NavBarProps) => {
         setExpanded(!expanded);
     }
 
-    return (
+    const NavBarContent = (
+        <Shadow distance={12} startColor={theme.COLORS.BLACK + "60"} offset={[0, 5]}>
+            <View
+                style={[
+                    styles.navBar,
+                    {
+                        width: expanded ? width * 0.6 : width * 0.1,
+                        height: height,
+                        backgroundColor: theme.COLORS.MAIN_BLUE,
+                        borderTopEndRadius: 25,
+                        borderBottomEndRadius: 25,
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        overflow: 'hidden',
+                    }
+                ]}
+            >
+                {expanded && (
+                    <View style={{ height: '100%', width: '85%', padding: 10, gap: 24 }}>
+                        <View>
+                            <Image source={logo} style={{ width: 40, height: 40 }} />
+                            <TouchableOpacity onPress={toggleTheme} style={{ position: "absolute", top: 10, right: 10 }}>
+                                <FontAwesomeIcon
+                                    icon={isDarkMode ? faSun : faMoon}
+                                    size={24}
+                                    style={{ color: theme.COLORS.WHITE }}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{ marginBottom: 26 }}>
+                            <Text style={{ color: theme.COLORS.WHITE, fontSize: 24, textDecorationLine: 'underline' }}>Estação da Luz</Text>
+                            <Text style={{ color: theme.COLORS.WHITE, fontSize: 20 }}>Operador Isaías</Text>
+                        </View>
+                        <View style={{ flexDirection: 'column', gap: 40 }}>
+                            <TouchableOpacity style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }} onPress={() => handleNavigation('notifications')}>
+                                <View style={{ width: 70, height: 70, backgroundColor: theme.COLORS.WHITE, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
+                                    <FontAwesomeIcon icon={faBell} size={32} style={{ color: theme.COLORS.BLACK }} />
+                                </View>
+                                <Text style={{ color: theme.COLORS.WHITE, fontSize: 16 }}>Notificações</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }} onPress={() => handleNavigation('cams')}>
+                                <View style={{ width: 70, height: 70, backgroundColor: theme.COLORS.WHITE, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
+                                    <FontAwesomeIcon icon={faVideo} size={32} style={{ color: theme.COLORS.BLACK }} />
+                                </View>
+                                <Text style={{ color: theme.COLORS.WHITE, fontSize: 16 }}>Câmeras</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }} onPress={() => handleNavigation('citizenCreation')}>
+                                <View style={{ width: 70, height: 70, backgroundColor: theme.COLORS.WHITE, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
+                                    <FontAwesomeIcon icon={faUserPlus} size={32} style={{ color: theme.COLORS.BLACK }} />
+                                </View>
+                                <Text style={{ color: theme.COLORS.WHITE, fontSize: 16, flexWrap: 'wrap', width: '60%', textAlign: 'justify' }}>Cadastrar Cidadão</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )}
+                <TouchableOpacity
+                    style={{
+                        height: '98%', width: '15%', justifyContent: 'center', alignItems: 'center',
+                        borderLeftWidth: expanded ? 2 : 0, borderColor: '#24249A',
+                    }}
+                    onPress={(e) => {
+                        e.stopPropagation(); // Previne o clique fora de fechar o menu
+                        toggleExpand();
+                    }}
+                >
+                    <FontAwesomeIcon
+                        icon={expanded ? faCaretLeft : faCaretRight}
+                        size={32}
+                        style={{ color: theme.COLORS.WHITE }}
+                    />
+                </TouchableOpacity>
+            </View>
+        </Shadow>
+    );
+
+    return expanded ? (
         <TouchableWithoutFeedback onPress={handleOutsideClick}>
             <View style={{ position: 'absolute', bottom: 0, left: 0, zIndex: 10, height: height, width: width }}>
-                <Shadow distance={12} startColor={theme.COLORS.BLACK + "60"} offset={[0, 5]}>
-                    <View
-                        style={[
-                            styles.navBar,
-                            {
-                                width: expanded ? width * 0.6 : width * 0.1,
-                                height: height,
-                                backgroundColor: theme.COLORS.MAIN_BLUE,
-                                borderTopEndRadius: 25,
-                                borderBottomEndRadius: 25,
-                                flexDirection: 'row',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                overflow: 'hidden',
-                            }
-                        ]}
-                    >
-                        {expanded && (
-                            <View style={{ height: '100%', width: '85%', padding: 10, gap: 24 }}>
-                                <View>
-                                    <Image source={logo} style={{ width: 40, height: 40 }} />
-                                    <TouchableOpacity onPress={toggleTheme} style={{ position: "absolute", top: 10, right: 10 }}>
-                                        <FontAwesomeIcon
-                                            icon={isDarkMode ? faSun : faMoon}
-                                            size={24}
-                                            style={{ color: theme.COLORS.WHITE }}
-                                        />
-                                    </TouchableOpacity>
-                                </View>
-                                <View style={{ marginBottom: 26 }}>
-                                    <Text style={{ color: theme.COLORS.WHITE, fontSize: 24, textDecorationLine: 'underline' }}>Estação da Luz</Text>
-                                    <Text style={{ color: theme.COLORS.WHITE, fontSize: 20 }}>Operador Isaías</Text>
-                                </View>
-                                <View style={{ flexDirection: 'column', gap: 40 }}>
-                                    <TouchableOpacity style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }} onPress={() => handleNavigation('notifications')}>
-                                        <View style={{ width: 70, height: 70, backgroundColor: theme.COLORS.WHITE, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
-                                            <FontAwesomeIcon icon={faBell} size={32} style={{ color: theme.COLORS.BLACK }} />
-                                        </View>
-                                        <Text style={{ color: theme.COLORS.WHITE, fontSize: 16 }}>Notificações</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }} onPress={() => handleNavigation('cams')}>
-                                        <View style={{ width: 70, height: 70, backgroundColor: theme.COLORS.WHITE, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
-                                            <FontAwesomeIcon icon={faVideo} size={32} style={{ color: theme.COLORS.BLACK }} />
-                                        </View>
-                                        <Text style={{ color: theme.COLORS.WHITE, fontSize: 16 }}>Câmeras</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }} onPress={() => handleNavigation('citizenCreation')}>
-                                        <View style={{ width: 70, height: 70, backgroundColor: theme.COLORS.WHITE, justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
-                                            <FontAwesomeIcon icon={faUserPlus} size={32} style={{ color: theme.COLORS.BLACK }} />
-                                        </View>
-                                        <Text style={{ color: theme.COLORS.WHITE, fontSize: 16, flexWrap: 'wrap', width: '60%', textAlign: 'justify' }}>Cadastrar Cidadão</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                        )}
-                        <TouchableOpacity
-                            style={{
-                                height: '98%', width: '15%', justifyContent: 'center', alignItems: 'center',
-                                borderLeftWidth: expanded ? 2 : 0, borderColor: '#24249A',
-                            }}
-                            onPress={(e) => {
-                                e.stopPropagation(); // Previne o clique fora de fechar o menu
-                                toggleExpand();
-                            }}
-                        >
-                            <FontAwesomeIcon
-                                icon={expanded ? faCaretLeft : faCaretRight}
-                                size={32}
-                                style={{ color: theme.COLORS.WHITE }}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                </Shadow>
+                {NavBarContent}
             </View>
         </TouchableWithoutFeedback>
+    ) : (
+        <View style={{ position: 'absolute', bottom: 0, left: 0, zIndex: 10 }}>
+            {NavBarContent}
+        </View>
     );
 }
 
